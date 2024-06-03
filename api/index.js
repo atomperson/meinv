6095ea317b95e6f6e2a47def5314073005b8a437
moment <2742681975@qq.com>
2024-06-03 15:01:25 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -4,6 +4,8 @@ export default (req, res) => {
   console.log('----- meizi project ----')
 
   console.log(puppeteer.configuration.cacheDirectory)
+
+  res.setHeader("Content-Type", "application/json")
   
   res.write(JSON.stringify({
     code: 200,
