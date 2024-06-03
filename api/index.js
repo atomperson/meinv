5cae3d04cdeabd6c443be514b41556f78d2d35d9
moment <2742681975@qq.com>
2024-06-03 15:17:43 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -5,11 +5,20 @@ export default (req, res) => {
 
   console.log(puppeteer.configuration.cacheDirectory)
 
-  res.setHeader("Content-Type", "application/json")
+  // res.setHeader("Content-Type", "application/json")
   
-  res.write(JSON.stringify({
-    code: 200,
-    data: '测试文本', // puppeteer.configuration.cacheDirectory,
-    msg: '成功'
-  }))
+  // res.write(JSON.stringify({
+  //   code: 200,
+  //   data: '测试文本', // puppeteer.configuration.cacheDirectory,
+  //   msg: '成功'
+  // }))
+
+  return {
+    body: JSON.stringify({
+      code: 200,
+      data: '测试文本',
+      msg: '成功'
+    }),
+    status: 200
+  }
 }
