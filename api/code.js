7cf4862d4164044b953eaef9410d9fea40d23118
moment <2742681975@qq.com>
2024-06-04 18:48:04 +0800

    Update code.js

--- api/code.js
+++ api/code.js
@@ -7,66 +7,37 @@ const chromePath = path.join(__dirname ,'chrome-headless-shell')
 
 const isExists = fs.existsSync(chromePath)
 
-const itemsInfo = []
 
 // console.log('---dir', fs.readdirSync(__dirname))
 
 // console.log('---dir-', fs.readdirSync(path.join(__dirname, '../')))
 
 function getAllFilesInfo(dirPath) {
+    const itemsInfo = []
     function traverseDirectory(currentPath) {
-        fs.readdir(currentPath, (err, items) => {
-            if(err) {
-                itemsInfo.push({ name: currentPath, path: currentPath, err: err })
-            } else {
-                for(const item of items) {
-                    const itemPath = path.join(currentPath, item)
-                    const stat = fs.statSync(itemPath)
-
-                    if (stat.isFile() || stat.isDirectory()) {
-                        itemsInfo.push({
-                            name: item,
-                            path: itemPath,
-                            size: stat.size,
-                            isDirectory: stat.isDirectory()
-                        })
-                    }
-             
-                    if (stat.isDirectory()) {
-                        traverseDirectory(itemPath)
-                    }
-                }
+        const items = fs.readdirSync(currentPath)
+        for (const item of items) {
+            const itemPath = path.join(currentPath, item);
+            const stat = fs.statSync(itemPath);
+     
+            if (stat.isFile() || stat.isDirectory()) {
+                itemsInfo.push({
+                    name: item,
+                    path: itemPath,
+                    size: stat.size,
+                    createdAt: stat.ctime,
+                    modifiedAt: stat.mtime,
+                    isDirectory: stat.isDirectory()
+                });
             }
-        })
-
-        traverseDirectory(dirPath)
-        
-        // const items = fs.readdir(currentPath, (err, items) => {
-        //     for (const item of items) {
-        //     const itemPath = path.join(currentPath, item);
-        //     const stat = fs.statSync(itemPath);
      
-        //     if (stat.isFile() || stat.isDirectory()) {
-        //         itemsInfo.push({
-        //             name: item,
-        //             path: itemPath,
-        //             size: stat.size,
-        //             createdAt: stat.ctime,
-        //             modifiedAt: stat.mtime,
-        //             isDirectory: stat.isDirectory()
-        //         });
-        //     }
-     
-        //     if (stat.isDirectory()) {
-        //         traverseDirectory(itemPath);
-        //     }
-        // }
+            if (stat.isDirectory()) {
+                traverseDirectory(itemPath);
+            }
+        }
  
-        // traverseDirectory(dirPath);
-        // })
-     
-        
-        // return itemsInfo;
+        traverseDirectory(dirPath);
+        return itemsInfo;
     }
 }
 
@@ -91,17 +62,13 @@ function getAllFilesInfo(dirPath) {
 
 
 export default async () => {
-    getAllFilesInfo(path.join(__dirname, '../'))
-
-    setTimeout(() => {
-        console.log(itemsInfo)
-    }, 5000)
+    console.log(getAllFilesInfo(path.join(__dirname, '../', 'opt')))
  
-  return {
-    body: JSON.stringify({ code: 200, data: 'xx--xx--xx', msg: '成功' }),
-    headers: {
-        'Content-Type': 'application/json;charset=UTF-8'
-    },
-    status: 200
-  }
+    return {
+        body: JSON.stringify({ code: 200, data: 'xx--xx--xx', msg: '成功' }),
+        headers: {
+            'Content-Type': 'application/json;charset=UTF-8'
+        },
+        status: 200
+    }
 }
