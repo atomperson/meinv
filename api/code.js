2a4391e729268c1cfe33edc710d630e70f9efcae
moment <2742681975@qq.com>
2024-06-05 15:27:50 +0800

    Update code.js

--- api/code.js
+++ api/code.js
@@ -1,13 +1,8 @@
-import chrome from 'chrome-aws-lambda'
+import { Chromeless } from 'chromeless'
 
-// const browser = BrowserLess()
-
-// console.log(browser)
-
-console.log(11)
+console.log(Chromeless)
 
 export default async () => {
-    console.log(await chrome.executablePath)
     return {
         body: JSON.stringify({ code: 200, data: 'xx--xx--xx', msg: '成功' }),
         headers: {
