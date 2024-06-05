1699c9b2ebc3c304b545ac9066143f6afc6b5e71
moment <2742681975@qq.com>
2024-06-05 14:21:18 +0800

    Update code.js

--- api/code.js
+++ api/code.js
@@ -1,69 +1,11 @@
-import fs from 'fs'
-import path from 'path'
-import childProcess from 'child_process'
-import puppeteer from 'puppeteer'
+import BrowserLess from 'browserless'
 
-const chromePath = path.join(__dirname ,'chrome-headless-shell')
-
-const isExists = fs.existsSync(chromePath)
-
-
-// console.log('---dir', fs.readdirSync(__dirname))
-
-// console.log('---dir-', fs.readdirSync(path.join(__dirname, '../')))
-
-function getAllFilesInfo(dirPath) {
-    const itemsInfo = []
-    function traverseDirectory(currentPath) {
-        const items = fs.readdirSync(currentPath)
-        for (const item of items) {
-            const itemPath = path.join(currentPath, item);
-            const stat = fs.statSync(itemPath);
-     
-            if (stat.isFile() || stat.isDirectory()) {
-                itemsInfo.push({
-                    name: item,
-                    path: itemPath,
-                    size: stat.size,
-                    createdAt: stat.ctime,
-                    modifiedAt: stat.mtime,
-                    isDirectory: stat.isDirectory()
-                });
-            }
-     
-            if (stat.isDirectory()) {
-                traverseDirectory(itemPath);
-            }
-        }
- 
-        traverseDirectory(dirPath);
-        return itemsInfo;
-    }
-}
-
-// function getChromePath() {
-//     if(!isExists) {
-//         const chrome = childProcess.execSync('npx @puppeteer/browsers install chrome-headless-shell').toString()
-//         console.log('---- install path ----')
-//         console.log(chrome)
-//     }
-//     return getAllFilesInfo(chromePath)
-// }
-
-// const folderAndFileList = getChromePath()
-
-// async function main() {
-//     const browserFetcher = puppeteer.createBrowserFetcher()
-
-//     const browserList = await browserFetcher.localRevisions()
-
-//     console.log(browserList)
-// }
-
+const browser = BrowserLess()
 
 export default async () => {
-    console.log(getAllFilesInfo(path.join(__dirname, '../', 'opt')))
- 
+
+    console.log(browser)
+     
     return {
         body: JSON.stringify({ code: 200, data: 'xx--xx--xx', msg: '成功' }),
         headers: {
